fef06cdc8ffc12aac64fb604749a8f219aca6a03
Daniel Ma <dma@planningcenter.com>
2021-12-06 21:15:57 -0800

    day 3, part 2

--- day3.test.ts
+++ day3.test.ts
@@ -1,13 +1,7 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
-import {
-  leastCommonBit,
-  mostCommonBit,
-  parseBinary,
-  processInput,
-} from "./day3.ts";
+import * as impl from "./day3.ts";
 
-Deno.test("parsing inputs", () => {
-  const input = `
+const exampleInput = `
 00100
 11110
 10110
@@ -20,9 +14,10 @@ Deno.test("parsing inputs", () => {
 11001
 00010
 01010
-`;
+`.trim();
 
-  const cleanInput = processInput(input);
+Deno.test("parsing inputs", () => {
+  const cleanInput = impl.processInput(exampleInput);
 
   assertEquals(cleanInput, [
     "011110011100",
@@ -34,39 +29,60 @@ Deno.test("parsing inputs", () => {
 });
 
 Deno.test("mostCommonBit", () => {
-  assertEquals(mostCommonBit("101"), "1");
-  assertEquals(mostCommonBit("011"), "1");
-  assertEquals(mostCommonBit("000"), "0");
+  assertEquals(impl.mostCommonBit("101"), "1");
+  assertEquals(impl.mostCommonBit("011"), "1");
+  assertEquals(impl.mostCommonBit("000"), "0");
 });
 
 Deno.test("part 1, example test", () => {
-  const input = `
-00100
-11110
-10110
-10111
-10101
-01111
-00111
-11100
-10000
-11001
-00010
-01010
-`;
+  const cleanInput = impl.processInput(exampleInput);
+  const mostCommonBits = cleanInput.map((r) => impl.mostCommonBit(r)).join("");
+  const leastCommonBits = cleanInput.map((r) => impl.leastCommonBit(r)).join(
+    "",
+  );
 
-  const cleanInput = processInput(input);
-  const mostCommonBits = cleanInput.map(mostCommonBit).join("");
-  const leastCommonBits = cleanInput.map(leastCommonBit).join("");
-
-  assertEquals(parseBinary(mostCommonBits) * parseBinary(leastCommonBits), 198);
+  assertEquals(
+    impl.parseBinary(mostCommonBits) * impl.parseBinary(leastCommonBits),
+    198,
+  );
 });
 
 Deno.test("part 1, real input", () => {
   const input = Deno.readTextFileSync("day3.input.txt");
-  const cleanInput = processInput(input);
-  const mostCommonBits = cleanInput.map(mostCommonBit).join("");
-  const leastCommonBits = cleanInput.map(leastCommonBit).join("");
+  const cleanInput = impl.processInput(input);
+  const mostCommonBits = cleanInput.map((r) => impl.mostCommonBit(r)).join("");
+  const leastCommonBits = cleanInput.map((r) => impl.leastCommonBit(r)).join(
+    "",
+  );
 
-  console.log(parseBinary(mostCommonBits) * parseBinary(leastCommonBits));
+  console.log(
+    impl.parseBinary(mostCommonBits) * impl.parseBinary(leastCommonBits),
+  );
+});
+
+Deno.test("part 2, oxygen rating", () => {
+  const rating = impl.oxyGeneratorRating(exampleInput);
+
+  assertEquals(impl.parseBinary(rating), 23);
+});
+
+Deno.test("part 2, co2 scrubber rating", () => {
+  const rating = impl.co2scrubberRating(exampleInput);
+
+  assertEquals(impl.parseBinary(rating), 10);
+});
+
+Deno.test("part 2, multiply them", () => {
+  const oxyRating = impl.oxyGeneratorRating(exampleInput);
+  const co2Rating = impl.co2scrubberRating(exampleInput);
+
+  assertEquals(impl.parseBinary(oxyRating) * impl.parseBinary(co2Rating), 230);
+});
+
+Deno.test("part 2, real input", () => {
+  const input = Deno.readTextFileSync("day3.input.txt")
+  const oxyRating = impl.oxyGeneratorRating(input);
+  const co2Rating = impl.co2scrubberRating(input);
+
+  console.log(impl.parseBinary(oxyRating) * impl.parseBinary(co2Rating));
 });
--- day3.ts
+++ day3.ts
@@ -14,19 +14,72 @@ export function processInput(lines: string) {
   return transpose(bitLists).map((x) => x.join(""));
 }
 
-export function mostCommonBit(bitList: string): "0" | "1" {
-  const zeroes = bitList.match(/0/g)?.length || 0
-  const ones = bitList.length - zeroes
+export function mostCommonBit(
+  bitList: string,
+  fallback: "0" | "1" | undefined = undefined,
+): "0" | "1" {
+  const zeroes = bitList.match(/0/g)?.length || 0;
+  const ones = bitList.length - zeroes;
 
-  return ones > zeroes ? "1" : "0"
+  if (ones === zeroes) {
+    if (fallback) return fallback;
+    else {
+      throw new Error("even number, but no fallback provided!");
+    }
+  } else {
+    return ones > zeroes ? "1" : "0";
+  }
 }
 
-export function leastCommonBit(bitList: string): "0" | "1" {
-  const other = mostCommonBit(bitList)
+function flipBit(bit: "0" | "1") { return bit === "0" ? "1" : "0" }
 
-  return other === "1" ? "0" : "1"
+export function leastCommonBit(
+  bitList: string,
+  fallback: "0" | "1" | undefined = undefined,
+): "0" | "1" {
+  const other = mostCommonBit(bitList, fallback && flipBit(fallback));
+
+  return flipBit(other);
 }
 
 export function parseBinary(bitList: string) {
-  return parseInt(bitList, 2)
+  return parseInt(bitList, 2);
+}
+
+function _oxyGeneratorRating(input: string[], digit = 0): string {
+  const importantBits = input.map((l) => l[digit]);
+  const mostCommon = mostCommonBit(importantBits.join(""), "1");
+  const usable = input.filter((n) => n[digit] === mostCommon);
+  // console.log("_oxyGeneratorRating", { input, digit, importantBits, mostCommon, usable });
+
+  if (usable.length === 1) {
+    return usable[0];
+  } else if (usable.length === 0) {
+    throw new Error("wat");
+  } else {
+    return _oxyGeneratorRating(usable, digit + 1);
+  }
+}
+
+export function oxyGeneratorRating(input: string) {
+  return _oxyGeneratorRating(input.trim().split("\n"));
+}
+
+function _co2scrubberRating(input: string[], digit = 0): string {
+  const importantBits = input.map((l) => l[digit]);
+  const leastCommon = leastCommonBit(importantBits.join(""), "0");
+  const usable = input.filter((n) => n[digit] === leastCommon);
+  // console.log("_co2scrubberRating", { input, digit, importantBits, leastCommon, usable });
+
+  if (usable.length === 1) {
+    return usable[0];
+  } else if (usable.length === 0) {
+    throw new Error("wat");
+  } else {
+    return _co2scrubberRating(usable, digit + 1);
+  }
+}
+
+export function co2scrubberRating(input: string) {
+  return _co2scrubberRating(input.trim().split("\n"));
 }
