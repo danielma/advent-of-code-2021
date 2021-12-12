fe0b088bb92486f4df16d5cfd73fb1fdda1f9817
Daniel Ma <dma@planningcenter.com>
2021-12-11 20:10:08 -0800

    day 6, part 2
    The trick is that you can group all the baby fish

--- day6.test.ts
+++ day6.test.ts
@@ -1,62 +1,82 @@
 import { assertEquals } from "https://deno.land/std@0.117.0/testing/asserts.ts";
 import * as impl from "./day6.ts";
 
-const exampleInput = `3,4,3,1,2`
+const exampleInput = `3,4,3,1,2`;
 
 function assertStep(sim: impl.FishSimulation, fishCycles: number[]) {
-  sim.step()
+  sim.step();
   // console.log(sim.fish)
-  assertEquals(sim.fish, fishCycles)
+  assertEquals(sim.fish.map((x) => x.cycle), fishCycles);
 }
 
 Deno.test("parsing inputs", () => {
   const lanternFish = impl.parseFish(exampleInput);
-  const sim = new impl.FishSimulation(lanternFish)
+  const sim = new impl.FishSimulation(lanternFish);
 
-  assertEquals(sim.fish, [3,4,3,1,2])
+  assertEquals(sim.fish.map((f) => f.cycle), [3, 4, 3, 1, 2]);
 
-  assertStep(sim, [2,3,2,0,1])
-  assertStep(sim, [1,2,1,6,0,8])
-})
+  assertStep(sim, [2, 3, 2, 0, 1]);
+  assertStep(sim, [1, 2, 1, 6, 0, 8]);
+});
+
+function sumSim(sim: impl.FishSimulation): number {
+  return sum(sim.fish.map(f => f.count))
+}
+
+function sum(list: number[]): number {
+  return list.reduce((a, b) => a + b);
+}
 
 Deno.test("part 1, example input", () => {
   const lanternFish = impl.parseFish(exampleInput);
-  const sim = new impl.FishSimulation(lanternFish)
+  const sim = new impl.FishSimulation(lanternFish);
 
   for (let i = 0; i < 18; i++) {
-    sim.step()
+    sim.step();
   }
 
-  assertEquals(sim.fish.length, 26)
-
+  assertEquals(sumSim(sim), 26);
 
   for (let i = 0; i < 62; i++) {
-    sim.step()
+    sim.step();
   }
 
-  assertEquals(sim.fish.length, 5934)
-})
+  assertEquals(sumSim(sim), 5934);
+});
 
 Deno.test("part 1, realInput", () => {
   const input = Deno.readTextFileSync("day6.input.txt");
 
-  const fish = impl.parseFish(input)
-  const sim = new impl.FishSimulation(fish)
-  
+  const fish = impl.parseFish(input);
+  const sim = new impl.FishSimulation(fish);
+
   for (let i = 0; i < 80; i++) {
     sim.step()
   }
 
-  console.log(sim.fish.length)
+  console.log(sumSim(sim));
 });
 
 Deno.test("part 2, example", () => {
-  const fish = impl.parseFish(exampleInput)
-  const sim = new impl.FishSimulation(fish)
-  
+  const fish = impl.parseFish(exampleInput);
+  const sim = new impl.FishSimulation(fish);
+
   for (let i = 0; i < 256; i++) {
     sim.step()
   }
 
-  assertEquals(sim.fish.length, 26984457539)
+  assertEquals(sumSim(sim), 26984457539);
+});
+
+Deno.test("part 2, realInput", () => {
+  const input = Deno.readTextFileSync("day6.input.txt");
+
+  const fish = impl.parseFish(input);
+  const sim = new impl.FishSimulation(fish);
+
+  for (let i = 0; i < 256; i++) {
+    sim.step()
+  }
+
+  console.log(sumSim(sim));
 });
--- day6.ts
+++ day6.ts
@@ -2,22 +2,25 @@ function parseTen(n: string) {
   return parseInt(n, 10);
 }
 
-type LanternFish = number
+type LanternFish = { cycle: number; count: number };
 
 const LanternFish = {
   initialAge: 8,
 
-  new(cycle = -1) {
-    return cycle === -1 ? LanternFish.initialAge : cycle
+  new(cycle = -1, count = 1) {
+    return { cycle: cycle === -1 ? LanternFish.initialAge : cycle, count };
   },
 
   step(fish: LanternFish): LanternFish[] {
-    const nextCycle = fish - 1;
+    const nextCycle = fish.cycle - 1;
 
     if (nextCycle === -1) {
-      return [6, LanternFish.new()];
+      return [{ ...fish, cycle: 6 }, {
+        ...fish,
+        cycle: LanternFish.initialAge,
+      }];
     } else {
-      return [nextCycle];
+      return [{ ...fish, cycle: nextCycle }];
     }
   },
 };
@@ -30,18 +33,27 @@ export class FishSimulation {
   }
 
   step() {
-    const originalFishLength = this.fish.length
+    const originalFishLength = this.fish.length;
+    const newFish = [];
 
     for (let i = 0; i < originalFishLength; i++) {
-      const [fishNextValue, fishBaby] = LanternFish.step(this.fish[i])
+      const [fishNextValue, fishBaby] = LanternFish.step(this.fish[i]);
 
-      this.fish[i] = fishNextValue
+      this.fish[i] = fishNextValue;
 
-      if (fishBaby) this.fish.push(fishBaby)
+      if (fishBaby) newFish.push(fishBaby);
+    }
+
+    if (newFish.length > 0) {
+      this.fish.push(
+        newFish.reduce((m, f) => ({ ...m, count: m.count + f.count })),
+      );
     }
   }
 }
 
 export function parseFish(rawInput: string) {
-  return rawInput.trim().split(",").map(parseTen).map(LanternFish.new);
+  return rawInput.trim().split(",").map(parseTen).map((n) =>
+    LanternFish.new(n)
+  );
 }
